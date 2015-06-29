f52b5e2c9b731d7a1cf5e01053f87cf129d5f036
Michael Duo Ling <duo.ling.cn@gmail.com>
2015-06-29 08:51:38 +0000

    .

--- ts36_211/core/tests/TestFrame.py
+++ ts36_211/core/tests/TestFrame.py
@@ -1,6 +1,9 @@
 import unittest
 from ..Frame import Frame, L1Config
+from ..Plotter import Plotter
 
 class TestMatrix(unittest.TestCase):
     def test_create(self):
         frame = Frame(L1Config())
+        plotter = Plotter(frame)
+        plotter.plot("test_temporary_plot_frame_blank.png")
