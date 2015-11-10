8a4adfee28d11cf54f83ff71225a2e77650a2fa9
Michael Duo Ling <duo.ling.cn@gmail.com>
2015-11-10 13:47:18 +0000

    Basic ASN.1 ENUMERATED, BIT_STRING, SEQUENCE

--- ts36_331/asn1.py
+++ ts36_331/asn1.py
@@ -12,21 +12,33 @@ def ENUMERATED(name, *choices):
     for i in range(len(choices)):
         d[choices[i]] = i
     d['length'] = ceil(log2(len(choices)))
-    def init(self, isRequired, value=0):
-        self.isRequired = isRequired
+    def init(self, value=0):
         self.value = 0
         self.set(value)
     d['__init__'] = init
     def __len(self):
         return self.__class__.length
     d['__len__'] = __len
+    def eq(self, enum):
+        return self.value == enum.value
+    d['__eq__'] = eq
     def set(self, value):
-        assert 0 <= value < 2**self.length
-        self.value = value
+        if isinstance(value, int):
+            assert 0 <= value < 2**self.length
+            self.value = value
+        elif isinstance(value, Bits):
+            assert value.length == self.length
+            self.value = value.uint
+        return self
     d['set'] = set
     def code(self):
         return Bits(uint=self.value, length=self.length)
     d['code'] = code
+    @classmethod
+    def decode(cls, bits):
+        assert isinstance(bits, Bits)
+        return cls().set(bits)
+    d['decode'] = decode
     return type(name, (object,), d)
 
 def BIT_STRING(name, length):
@@ -34,13 +46,15 @@ def BIT_STRING(name, length):
     assert length > 0
     d = dict()
     d['length'] = length
-    def init(self, isRequired, bits=0):
-        self.isRequired = isRequired
+    def init(self, bits=0):
         self.set(bits)
     d['__init__'] = init
     def _len(self):
         return self.__class__.length
     d['__len__'] = _len
+    def eq(self, bitstring):
+        return self.bits == bitstring.bits
+    d['__eq__'] = eq
     def set(self, bits):
         if isinstance(bits, int):
             assert 0 <= bits < 2**self.length
@@ -55,56 +69,70 @@ def BIT_STRING(name, length):
                 bits = b'0' * ceil((self.length - 8*len(bits))/8) +  bits
             bits = Bits(bytes=bits, length=self.length, offset=len(bits)*8-self.length)
         self.bits = bits
+        return self
     d['set'] = set
     def code(self):
         return self.bits
     d['code'] = code
+    @classmethod
+    def decode(cls, bits):
+        assert isinstance(bits, Bits)
+        return cls().set(bits)
+    d['decode'] = decode
     return type(name, (object,), d)
 
 def SEQUENCE(name, *members):
     assert len(members) > 0
     for member in members:
-        print(member)
         assert not isinstance(member, type) # should not be a class
     d = dict()
     l = list()
     length = 0
     for member in members:
-        d[str(member.__class__).split('.')[-1]] = member
+        d[str(member.__class__).split('.')[-1][:-2]] = member
         l.append(member)
         length += len(member)
     d['length'] = length
     d['members'] = l
-    return type(name, (object,), d)
-'''
-class BIT_STRING:
-    def __init__(self, length):
-        assert isinstance(length, int) and length > 0
-        self.length = length
-        self.value = 0
-    def __len__(self):
-        return self.length
-    def code(self, v):
-        assert isinstance(v, int) and v >= 0 and v < 2**self.length
-        return Bits(uint=v, length=self.length)
-
-class SEQUENCE:
-    def __init__(self, *args):
-        assert len(args) > 0
-        self.values = list()
-        for attribute in args:
-            assert isinstance(attribute, Bits)
-                or isinstance(attribute, ENUMERATED)
-                or isinstance(attribute, BIT_STRING)
-                or isinstance(attribute, SEQUENCE)
-            self.values.append(attribute)
-    def __len__(self):
-        length = 0
-        for v in self.values:
-            length += len(v)
-        return length
     def code(self):
-'''
+        bits = self.members[0].code()
+        for member in self.members[1:]:
+            bits += member.code()
+        return bits
+    d['code'] = code
+    def _len(self):
+        return self.__class__.length
+    d['__len__'] = _len
+    def eq(self, seq):
+        result = True
+        for i in range(len(self.members)):
+            result = result and self.members[i] == seq.members[i]
+            if not result: break
+        return result
+    d['__eq__'] = eq
+    @classmethod
+    def decode(cls, bits):
+        assert isinstance(bits, Bits)
+        seq = cls()
+        index = 0
+        for member in seq.members:
+            member.set(bits[index:index+member.length])
+            index += member.length
+        return seq
+    d['decode'] = decode
+    return type(name, (object,), d)
 
 
-dl_Bandwidth = ENUMERATED('n6', 'n15', 'n25', 'n50', 'n75', 'n100')
+Dl_Bandwidth = ENUMERATED('Dl_Bandwidth', 'n6', 'n15', 'n25', 'n50', 'n75', 'n100')
+Phich_Duration = ENUMERATED('Phich_Duration', 'normal', 'extended')
+Phich_Resource = ENUMERATED('Phich_Resource', 'oneSixth', 'half', 'one', 'two')
+PHICH_Config = SEQUENCE('PHICH_Config', Phich_Duration(), Phich_Resource())
+SystemFrameNumber = BIT_STRING('SystemFrameNumber', 8)
+MasterInformationBlock = SEQUENCE('MasterInformationBlock', Dl_Bandwidth(), PHICH_Config(), SystemFrameNumber(), BIT_STRING('Spare', 10)())
+
+
+
+
+
+
+
--- ts36_331/tests/TestAsn1.py
+++ ts36_331/tests/TestAsn1.py
@@ -1,6 +1,6 @@
 import unittest
 from nose.tools import eq_
-from ..asn1 import ENUMERATED, BIT_STRING, SEQUENCE
+from ..asn1 import ENUMERATED, BIT_STRING, SEQUENCE, Dl_Bandwidth, Phich_Duration, Phich_Resource, PHICH_Config, SystemFrameNumber, MasterInformationBlock
 from ...misc.bitstring.bitstring import Bits
 
 class TestAsn1(unittest.TestCase):
@@ -8,37 +8,92 @@ class TestAsn1(unittest.TestCase):
         E = ENUMERATED('Enum', 'zero', 'one', 'two', 'three', 'four')
         eq_(E.__name__, 'Enum')
         eq_(E.length, 3)
-        e = E(False)
+        e = E()
         eq_(e.code(), Bits(uint=0, length=3))
+        eq_(E.decode(e.code()), e)
         e.set(E.three)
         eq_(e.code(), Bits(uint=3, length=3))
 
     def test_BIT_STRING(self):
         BS = BIT_STRING('BitString', 9)
-        bs = BS(True)
+        bs = BS()
         bs.set(Bits(uint=11, length=9))
         eq_(bs.code(), Bits(uint=11, length=9))
         bs.set(11)
         eq_(bs.code(), Bits(uint=11, length=9))
+        eq_(BS.decode(bs.code()), bs)
         bs.set(b'a')
         eq_(bs.code(), Bits(bin='0b001100001'))
 
     def test_SEQUENCE(self):
         A = ENUMERATED('A', 'zero', 'one', 'two', 'three')
         B = BIT_STRING('B', 6)
-        Seq = SEQUENCE('SEQ', A(True), B(False))
+        Seq = SEQUENCE('SEQ', A(), B())
+        s = Seq()
+        s.A.set(A.three)
+        s.B.set(0b010101)
+        eq_(s.code(), Bits(bin='0b11010101'))
+        eq_(Seq.decode(s.code()), s)
 
-    '''def test_MIB(self):
-        eq_(len(dl_Bandwidth), 3)
-        eq_(dl_Bandwidth.n6, 0)
-        eq_(dl_Bandwidth.n15, 1)
-        eq_(dl_Bandwidth.n25, 2)
-        eq_(dl_Bandwidth.n50, 3)
-        eq_(dl_Bandwidth.n75, 4)
-        eq_(dl_Bandwidth.n100, 5)
-        eq_(dl_Bandwidth.code('n15'), Bits(int=1, length=3))
-        eq_(dl_Bandwidth.code('n6'), Bits(int=0, length=3))
-        eq_(dl_Bandwidth.code('n25'), Bits(int=2, length=3))
-        eq_(dl_Bandwidth.code('n50'), Bits(int=3, length=3))
-        eq_(dl_Bandwidth.code('n75'), Bits(uint=4, length=3))
-        eq_(dl_Bandwidth.code('n100'), Bits(uint=5, length=3))'''
+    def test_Dl_Bandwidth(self):
+        eq_(Dl_Bandwidth.length, 3)
+        eq_(Dl_Bandwidth.n6, 0)
+        eq_(Dl_Bandwidth.n15, 1)
+        eq_(Dl_Bandwidth.n25, 2)
+        eq_(Dl_Bandwidth.n50, 3)
+        eq_(Dl_Bandwidth.n75, 4)
+        eq_(Dl_Bandwidth.n100, 5)
+
+        eq_(Dl_Bandwidth(Dl_Bandwidth.n6).code(), Bits(int=0, length=3))
+        eq_(Dl_Bandwidth(Dl_Bandwidth.n15).code(), Bits(int=1, length=3))
+        eq_(Dl_Bandwidth(Dl_Bandwidth.n25).code(), Bits(int=2, length=3))
+        eq_(Dl_Bandwidth(Dl_Bandwidth.n50).code(), Bits(int=3, length=3))
+        eq_(Dl_Bandwidth(Dl_Bandwidth.n75).code(), Bits(uint=4, length=3))
+        eq_(Dl_Bandwidth(Dl_Bandwidth.n100).code(), Bits(uint=5, length=3))
+
+    def test_Phich_Duration(self):
+        eq_(Phich_Duration.length, 1)
+        eq_(Phich_Duration.normal, 0)
+        eq_(Phich_Duration.extended, 1)
+
+        eq_(Phich_Duration(Phich_Duration.normal).code(), Bits(bin='0b0'))
+        eq_(Phich_Duration(Phich_Duration.extended).code(), Bits(bin='0b1'))
+
+    def test_Phich_Resource(self):
+        eq_(Phich_Resource.length, 2)
+        eq_(Phich_Resource.oneSixth, 0)
+        eq_(Phich_Resource.half, 1)
+        eq_(Phich_Resource.one, 2)
+        eq_(Phich_Resource.two, 3)
+
+        eq_(Phich_Resource(Phich_Resource.oneSixth).code(), Bits(bin='0b00'))
+        eq_(Phich_Resource(Phich_Resource.half).code(), Bits(bin='0b01'))
+        eq_(Phich_Resource(Phich_Resource.one).code(), Bits(bin='0b10'))
+        eq_(Phich_Resource(Phich_Resource.two).code(), Bits(bin='0b11'))
+
+    def test_PHICH_Config(self):
+        eq_(PHICH_Config.length, 3)
+
+        p = PHICH_Config()
+        p.Phich_Duration.set(Phich_Duration.extended)
+        p.Phich_Resource.set(Phich_Resource.one)
+        eq_(p.code(), Bits(bin='0b110'))
+
+    def test_SystemFrameNumber(self):
+        eq_(SystemFrameNumber.length, 8)
+
+        s = SystemFrameNumber()
+        s.set(Bits(bin='0b01010101'))
+        eq_(s.code(), Bits(bin='0b01010101'))
+
+    def test_MasterInformationBlock(self):
+        eq_(MasterInformationBlock.length, 24)
+        mib = MasterInformationBlock()
+        mib.PHICH_Config.Phich_Duration.set(Phich_Duration.extended)
+        mib.PHICH_Config.Phich_Resource.set(Phich_Resource.half)
+        mib.Dl_Bandwidth.set(Dl_Bandwidth.n25)
+        mib.SystemFrameNumber.set(0xef)
+        eq_(mib.code(), Bits(bin='0b010101111011110000000000'))
+
+
+
